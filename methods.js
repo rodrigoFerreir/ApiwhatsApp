2313264e92751a67bca0e0ffd32956ce294d6ba2
Rodrigo Ferreira <rodrigoferreirasilva1997@hotmail.com>
2020-02-28 11:49:27 -0300

    corrigindo sendMessage()

--- methods.js
+++ methods.js
@@ -2,14 +2,36 @@ const puppeteer = require('puppeteer');
 
 let pages = [];
 module.exports = {
+
+    async test(req, res) {
+        console.log('k')
+        const wsChromeEndpointurl = 'ws://127.0.0.1:9222/devtools/browser/18ed15dc-3977-4d65-9bab-30399f8c085e';
+        const browser = await puppeteer.connect({
+            browserWSEndpoint: wsChromeEndpointurl,
+            headless: true,
+
+        });
+        console.log('k')
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/inde.html')
+        console.log('x')
+        //await page.$eval('#butto', elem => elem.click());
+        console.log('xx')
+        pages = await browser.pages();
+        res.send('ok');
+        return pages
+    },
+
+    async testClick(req, res){
+        console.log(pages[1])
+        let page = pages[1]
+        await page.$eval('#butto', elem => elem.click());
+        res.send('ok click')
+    },
+
     async openPage(req, res) {
         const browser = await puppeteer.launch({
             headless: false,
-            args: [
-                '--disable-background-timer-throttling',
-                '--disable-backgrounding-occluded-windows',
-                '--disable-renderer-backgrounding'
-            ],
         });
         const page = await browser.newPage();
         await page.goto('https://web.whatsapp.com/')
@@ -26,6 +48,7 @@ module.exports = {
         await page.goto(
             `https://web.whatsapp.com/send?phone=${code}${phone}`
         );
+
         try {
             await page.waitForNavigation();
         } catch (e) {
@@ -36,9 +59,12 @@ module.exports = {
                 return false;
             });
         }
+
         await page.type("div._3u328", message);
         await page.$(`span[data-icon='send']`);
-        await page.click("span[data-icon='send']");
+        await page.waitFor(1000);
+        await page.$eval(`span[data-icon='send']`, elem => elem.click());
+        //await page.click("span[data-icon='send']");
         await page.on("dialog", async dialog => {
             try {
                 await dialog.accept();
@@ -49,6 +75,20 @@ module.exports = {
             status: 'Mensagem enviada!'
         })
     },
+
+    // async marcandoMsgLida(req, res){
+    //     const page = pages[1];
+    //     let arrayMsg = await page.evaluate(() => Array.from(document.querySelectorAll('div.X7YrQ div._2WP9Q')).map(patern => {
+    //         return patern
+    //     }))
+    //     arrayMsg.map((i, index) => {
+    //         if (arrayMsg) {
+    //             arrayMsg[index].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[0].innerHTML = "";
+    //         } else {
+    //             res.status(200).send('Sem novas mensagens')
+    //         };
+    //     });
+    // },
 
     async getMessages(req, res) {
         const page = pages[1];
@@ -74,8 +114,7 @@ module.exports = {
         res.status(200).json({
             messages: msg
         })
-
-        console.log(msg)
+        //this.marcandoMsgLida();
         return msg;
-    }
+    },
 }
--- routes.js
+++ routes.js
@@ -2,7 +2,11 @@ const { Router } = require('express')
 const routes = Router();
 const Methods = require('./methods')
 
-  routes.get('/teste', Methods.openPage);
+  routes.get('/teste', Methods.test);
+  //routes.post('/teste', Methods.testClick)
+
+  routes.get('/', Methods.openPage);
+
   routes.post('/teste', Methods.sendMessage);
 
   routes.get('/message', Methods.getMessages)
